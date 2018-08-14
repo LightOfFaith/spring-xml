e6c0f54ee33ec69ab51db27128d8b57e31f0c2b6
share <share@lifetime.com>
2018-08-14 18:20:00 +0800

    Signed-off-by: share <share@lifetime.com> Change-Id: I0000000000000000000000000000000000000002

---
+++ src/test/java/com/share/lifetime/util/StringUtilsTest.java
@@ -0,0 +1,14 @@
+package com.share.lifetime.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
