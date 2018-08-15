5557112a880bfc58a2289e1ad7789e73ac39ef0a
share <share@lifetime.com>
2018-08-15 17:00:00 +0800

    Signed-off-by: share <share@lifetime.com> Change-Id: I0000000000000000000000000000000000000004

--- src/main/java/com/share/lifetime/util/WebUtils.java
+++ src/main/java/com/share/lifetime/util/WebUtils.java
@@ -1,5 +1,173 @@
 package com.share.lifetime.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class WebUtils {
+
+	private static final String DEFAULT_CHARSET = "UTF-8";
+
+	/**
+	 * 获取客户端实际ip
+	 * @param request
+	 * @return
+	 */
+	public static String getAddr(HttpServletRequest request) {
+		String ip = request.getHeader("X-Real-IP");
+
+		if (ip != null && ip.length() != 0 && !"unKnown".equalsIgnoreCase(ip)) {
+			return ip;
+		}
+		ip = request.getHeader("X-Forwarded-For");
+
+		if (ip != null && ip.length() != 0 && !"unKnown".equalsIgnoreCase(ip)) {
+			// 多次反向代理后会有多个ip值，第一个ip才是真实ip
+			int index = ip.indexOf(",");
+			if (index != -1) {
+				return ip.substring(0, index);
+			} else {
+				return ip;
+			}
+		}
+
+		return request.getRemoteAddr();
+	}
+
+	/**
+	 * 使用默认的UTF-8字符集反编码请求参数值。
+	 * 
+	 * @param value 参数值
+	 * @return 反编码后的参数值
+	 */
+	public static String decode(String value) {
+		return decode(value, DEFAULT_CHARSET);
+	}
+
+	/**
+	 * 使用默认的UTF-8字符集编码请求参数值。
+	 * 
+	 * @param value 参数值
+	 * @return 编码后的参数值
+	 */
+	public static String encode(String value) {
+		return encode(value, DEFAULT_CHARSET);
+	}
+
+	/**
+	 * 使用指定的字符集反编码请求参数值。
+	 * 
+	 * @param value 参数值
+	 * @param charset 字符集
+	 * @return 反编码后的参数值
+	 */
+	public static String decode(String value, String charset) {
+		String result = null;
+		if (!StringUtils.isEmpty(value)) {
+			try {
+				result = URLDecoder.decode(value, charset);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 使用指定的字符集编码请求参数值。
+	 * 
+	 * @param value 参数值
+	 * @param charset 字符集
+	 * @return 编码后的参数值
+	 */
+	public static String encode(String value, String charset) {
+		String result = null;
+		if (!StringUtils.isEmpty(value)) {
+			try {
+				result = URLEncoder.encode(value, charset);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return result;
+	}
+
+	public static String buildQuery(Map<String, String> params, String charset) throws IOException {
+		if (params == null || params.isEmpty()) {
+			return null;
+		}
+
+		StringBuilder query = new StringBuilder();
+		Set<Entry<String, String>> entries = params.entrySet();
+		boolean hasParam = false;
+
+		for (Entry<String, String> entry : entries) {
+			String name = entry.getKey();
+			String value = entry.getValue();
+			// 忽略参数名或参数值为空的参数
+			if (StringUtils.areNotEmpty(name, value)) {
+				if (hasParam) {
+					query.append("&");
+				} else {
+					hasParam = true;
+				}
+
+				query.append(name).append("=").append(URLEncoder.encode(value, charset));
+			}
+		}
+
+		return query.toString();
+	}
+
+	public static String getStreamAsString(InputStream stream, String charset) throws IOException {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream, charset));
+			StringWriter writer = new StringWriter();
+
+			char[] chars = new char[256];
+			int count = 0;
+			while ((count = reader.read(chars)) > 0) {
+				writer.write(chars, 0, count);
+			}
+
+			return writer.toString();
+		} finally {
+			if (stream != null) {
+				stream.close();
+			}
+		}
+	}
+
+	/**
+	 * 从URL中提取所有的参数。
+	 * 
+	 * @param query URL地址
+	 * @return 参数映射
+	 */
+	public static Map<String, String> splitUrlQuery(String query) {
+		Map<String, String> result = new HashMap<String, String>();
+
+		String[] pairs = query.split("&");
+		if (pairs != null && pairs.length > 0) {
+			for (String pair : pairs) {
+				String[] param = pair.split("=", 2);
+				if (param != null && param.length == 2) {
+					result.put(param[0], param[1]);
+				}
+			}
+		}
+
+		return result;
+	}
 
 }
