853afc1edef9078902af1f7cd1bad1b329c61b37
share <share@lifetime.com>
2018-09-05 18:18:00 +0800

    1.1.4

--- src/main/java/com/share/lifetime/util/SignatureUtils.java
+++ src/main/java/com/share/lifetime/util/SignatureUtils.java
@@ -1,5 +1,6 @@
 package com.share.lifetime.util;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,7 +11,7 @@ public class SignatureUtils {
 
 	public static final String CHARSET_UTF8 = "UTF-8";
 
-	private static final String ALGORITHM_MD5 = "MD5";
+	public static final String SIGN_TYPE_MD5 = "MD5";
 
 	public static final String SIGN = "sign";
 
@@ -79,6 +80,62 @@ public class SignatureUtils {
 		}
 
 		return content.toString();
+	}
+
+	/**
+	 * 加签
+	 * @param params
+	 * @param salt
+	 * @param charset
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	public static String md5Sign(Map<String, String> params, String salt, String charset)
+			throws UnsupportedEncodingException {
+		String signContent = getSignContent(params);
+		return EncryptUtils.encryptMD5ToString(signContent, salt, charset);
+	}
+
+	private static String md5Sign(String content, String salt, String charset) throws UnsupportedEncodingException {
+		return EncryptUtils.encryptMD5ToString(content, salt, charset);
+	}
+
+	/**
+	 * 验签
+	 * @param content
+	 * @param sign
+	 * @param salt
+	 * @param charset
+	 * @param signType
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	public static boolean checkSignV2(Map<String, String> params, String salt, String charset, String signType)
+			throws UnsupportedEncodingException {
+		String sign = params.get("sign");
+		String content = getSignCheckContentV2(params);
+
+		return checkSignV2(content, sign, salt, charset, signType);
+	}
+
+	private static boolean checkSignV2(String content, String sign, String salt, String charset, String signType)
+			throws UnsupportedEncodingException {
+
+		if (SIGN_TYPE_MD5.equals(signType)) {
+
+			return md5SignVerifyV2(content, salt, charset, sign);
+
+		} else {
+
+			throw new RuntimeException("Sign Type is Not Support : signType=" + signType);
+		}
+
+	}
+
+	private static boolean md5SignVerifyV2(String content, String salt, String charset, final String sign)
+			throws UnsupportedEncodingException {
+		String md5Sign = md5Sign(content, salt, charset);
+		return md5Sign.equals(sign);
 	}
 
 
